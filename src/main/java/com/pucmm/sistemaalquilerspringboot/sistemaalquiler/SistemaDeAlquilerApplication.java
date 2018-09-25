af025d4b278d6d0b0c17470b531f2c188c716fc7
Eric Nunez <ericlavega96@gmail.com>
2018-09-25 10:33:28 -0400

    Subiendo partes del proyecto

---
+++ src/main/java/com/pucmm/sistemaalquilerspringboot/sistemaalquiler/SistemaDeAlquilerApplication.java
@@ -0,0 +1,12 @@
+package com.pucmm.sistemaalquilerspringboot.sistemaalquiler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeAlquilerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeAlquilerApplication.class, args);
+	}
+}
