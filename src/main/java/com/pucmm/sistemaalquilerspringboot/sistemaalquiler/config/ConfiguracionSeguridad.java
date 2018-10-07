8dc250a0fae3cd0573e43102ef309bbfaa3d418a
Eric Nunez <ericlavega96@gmail.com>
2018-10-07 01:19:10 -0400

    Subiendo arreglo a tablas para mostrar preview de las fotos de clientes

--- src/main/java/com/pucmm/sistemaalquilerspringboot/sistemaalquiler/config/ConfiguracionSeguridad.java
+++ src/main/java/com/pucmm/sistemaalquilerspringboot/sistemaalquiler/config/ConfiguracionSeguridad.java
@@ -3,12 +3,15 @@ package com.pucmm.sistemaalquilerspringboot.sistemaalquiler.config;
 import com.pucmm.sistemaalquilerspringboot.sistemaalquiler.servicios.serviciosEntidades.seguridad.ServiciosUsuario;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configurable
 @EnableGlobalMethodSecurity(securedEnabled = true)
@@ -33,7 +36,7 @@ public class ConfiguracionSeguridad extends WebSecurityConfigurerAdapter{
         //Marcando las reglas de autorizacion
         http
                 .authorizeRequests()
-                .antMatchers("/","/css/**", "/js/**","/lib/**").permitAll() //permitiendo llamadas a esas urls.
+                .antMatchers("/","/css/**","/lib/**").permitAll() //permitiendo llamadas a esas urls.
                 .antMatchers("/dbconsole/**").permitAll()
                 .antMatchers("/admin/**").hasAnyRole("ADMIN", "USER")
                 .and()
@@ -51,6 +54,14 @@ public class ConfiguracionSeguridad extends WebSecurityConfigurerAdapter{
         //Necesario para H2.
         http.csrf().disable();
         http.headers().frameOptions().disable();
+    }
+
+    @Configuration
+    public class AdditionalResourceWebConfiguration implements WebMvcConfigurer {
+        @Override
+        public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+            registry.addResourceHandler("/images/**").addResourceLocations("file:images/");
+        }
     }
 
 
